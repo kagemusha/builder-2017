d2b2fa63b529a4f8ba7bc61c54186c02f5eaf90c
kagemusha <mmadrid99@gmail.com>
2016-11-16 00:20:24 -0500

    read html file locally

--- app/components/template-slicer/component.js
+++ app/components/template-slicer/component.js
@@ -4,6 +4,22 @@ import uniqloTemplate from '../../example_emails/uniqlo';
 
 const { computed } = Ember;
 
+function getFileReadPromise(file) {
+
+  const promise = new Ember.RSVP.Promise(function(resolve){
+
+    const reader = new FileReader();
+    reader.onload = function(readEvent){
+      resolve({name: file.name, html: readEvent.target.result});
+    };
+    reader.readAsBinaryString(file);
+
+  });
+
+  return promise;
+}
+
+
 export default Ember.Component.extend({
 
   templates: [meetupTemplate, uniqloTemplate],
@@ -34,6 +50,13 @@ export default Ember.Component.extend({
     templateChanged(templateNum) {
       this.set('template', this.get('templates')[templateNum]);
       this.set('savedSlices', []);
+    },
+    didSelectFile(files, resetInput) {
+      const fileReader = getFileReadPromise(files[0]);
+      fileReader.then(file => {
+        this.set("template", file);
+        resetInput();
+      })
     }
   }
 });
